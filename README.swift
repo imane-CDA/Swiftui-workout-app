9aa0e1b8ec091fa8f3170b6dc564614b278f3963
Boujnane imane <boujnane.imaane@gmail.com>
2026-03-27 23:55:29 +0100

    Update README.swift
    README VF

--- README.swift
+++ README.swift
@@ -1,11 +1,11 @@
-# 🏋️ SwiftUI Workout App
+🏋️ SwiftUI Workout App
 
-Application iOS développée en **SwiftUI** permettant d’afficher une liste de workouts avec navigation vers une vue de détail.  
-Projet réalisé dans le cadre d’une formation, basé sur l’architecture **MVVM** et les bonnes pratiques de développement.
+Application iOS développée en SwiftUI permettant d’afficher une liste de workouts avec navigation vers une vue de détail.  
+Projet réalisé dans le cadre d’une formation, basé sur l’architecture MVVM et les bonnes pratiques de développement.
 
 ---
 
-## 📱 Fonctionnalités
+📱 Fonctionnalités
 
 - Affichage d’une liste de workouts
 - Carte personnalisée pour chaque exercice
@@ -19,9 +19,9 @@ Projet réalisé dans le cadre d’une formation, basé sur l’architecture **M
 
 ---
 
-## 🧱 Architecture
+🧱 Architecture
 
-Le projet suit l’architecture **MVVM** :
+Le projet suit l’architecture MVVM :
 
 workouts
 ↓
@@ -35,20 +35,20 @@ WorkoutDetailView
 
 ---
 
-## ⚙️ Concepts SwiftUI utilisés
+⚙️ Concepts SwiftUI utilisés
 
-- `ZStack`
-- `VStack` / `HStack`
-- `NavigationStack`
-- `NavigationLink`
-- `ForEach`
-- `Identifiable`
+- ZStack
+- VStack / HStack
+- NavigationStack
+- NavigationLink
+- ForEach
+- Identifiable
 
 ---
 
-## ✅ Bonnes pratiques
+✅ Bonnes pratiques
 
-- Utilisation de couleurs adaptatives (`.primary`, `.secondary`)
+- Utilisation de couleurs adaptatives (.primary, .secondary)
 - Computed properties dans les enums
 - Séparation des responsabilités (MVVM)
 
